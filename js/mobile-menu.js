bd914db1286c969378a0de121a73a6926cd33136
evgeniy210191 <bees127@ukr.net>
2023-01-25 12:00:28 +0200

    first commit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,11 @@
-const [nav] = ['button-nav']
+const [nav, closeMenu] = ['button-nav', 'cont']
 .map(item => document.getElementById(item))
 
 nav.onclick = function (event) {
   nav.classList.toggle('is-open');
   document.querySelector('body').classList.toggle('hidden')
 }
+closeMenu.onclick = function (event) {
+  nav.classList.remove('is-open');
+  document.querySelector('body').classList.remove('hidden')
+};
